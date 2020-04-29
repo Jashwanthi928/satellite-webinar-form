30ac3258cfbdba3fe1c0eb628d0cb1c194c8469a
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2020-04-29 19:42:35 +0530

    git add .

---
+++ script.js
@@ -0,0 +1,55 @@
+
+    $.get('./colleges.json', function colleges(data) {
+     
+        data.forEach((college,i) => {
+       var x= document.createElement('option')
+       var parent=document.getElementById('colleges-datalist');
+        parent.appendChild(x);
+        x.id=i;
+        x.value=college;
+       }); 
+       console.log(data) 
+       $.get('./collegelist.json', function colleges(datalist) {
+           console.log(datalist)
+       $(document).ready(function() {
+        // process the form
+        $('form').submit(function(event) {
+    
+            // get the form data
+            // there are many ways to get this data using jQuery (you can use the class or id also)
+            var formData = {
+                'name'              : $('input[name=name]').val(),
+                'email'             : $('input[name=email]').val(),
+                'phone'    : $('input[name=phone]').val(),
+                'stream'    : $('input[name=stream]').val(),
+                'college'    : $('input[name=college]').val()
+            };
+    
+            // process the form
+            $.ajax({
+                type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+                url         : 'https://staging-mern-endpoint.herokuapp.com/webinar.php', // the url where we want to POST
+                data        : formData, // our data object
+                dataType    : 'json', // what type of data do we expect back from the server
+                            encode          : true
+            })
+
+                // using the done promise callback
+                .done(function(college_data) {
+                    // log data to the console so we can see
+                    console.log(JSON.stringify(college_data.college)); 
+                    console.log(data);
+                   function checkindex(college){
+                       return college==college_data.college;
+                   }
+                   var index=data.findIndex(checkindex);
+                    location.href = datalist[index]
+                    console.log(datalist[index])
+                    // here we will handle errors and validation messages
+                });
+            // stop the form from submitting the normal way and refreshing the page
+            event.preventDefault();
+        });
+     })
+});
+});
